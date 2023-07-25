1c15f4dfc74eaac91a51b99a18b19c9a5d2d8958
JooHyeonKim <subin_282@naver.com>
2023-07-25 14:22:57 +0900

    stack 뿌시기

--- Stack/10828_스택.py
+++ Stack/10828_스택.py
@@ -1,3 +1,35 @@
 import sys
 
-n = i
+input = sys.stdin.readline
+n = int(input())
+
+stack = []
+
+for _ in range(n):
+    command = input().split()
+
+    if command[0] == "push":
+        stack.append(command[1])
+
+    elif command[0] == "pop":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())  #pop한 수 출력
+
+    elif command[0] == "size":
+        print(len(stack))
+
+    elif command[0] == "empty":
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+
+    elif command[0] == "top":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
+
+
